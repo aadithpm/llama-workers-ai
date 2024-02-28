b962bd8f727068f24dee5291a670c5459f518c85
Aadith PM <aadithpm@gmail.com>
2024-02-27 22:40:25 -0600

    :sparkles: Allow users to POST a prompt

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,33 @@
 import { Ai } from './vendor/@cloudflare/ai.js';
 
+export type Chat = {
+	messages: Array<{ role: string; content: string }>;
+};
+
 export default {
 	async fetch(request, env) {
-		const tasks = [];
+		const tasks: Array<{ inputs: Chat; response: any }> = [];
 		const ai = new Ai(env.AI);
+		if (request?.method == 'POST') {
+			const body = await request
+				.json()
+				.then((j: any) => j as any)
+				.catch((e: any) => {
+					console.log('error: ', e);
+				});
 
-		// prompt - simple completion style input
-		let simple = {
-			prompt: 'Tell me a joke about Cloudflare',
-		};
-		let response = await ai.run('@cf/meta/llama-2-7b-chat-int8', simple);
-		tasks.push({ inputs: simple, response });
-
-		// messages - chat style input
-		let chat = {
-			messages: [
-				{ role: 'system', content: 'You are a helpful assistant.' },
-				{ role: 'user', content: request?.prompt },
-			],
-		};
-		response = await ai.run('@cf/meta/llama-2-7b-chat-int8', chat);
-		tasks.push({ inputs: chat, response });
-
-		return Response.json(tasks);
+			if (body?.prompt?.length > 0) {
+				let chat: Chat = {
+					messages: [
+						{ role: 'system', content: 'You are a nerdy AI from a space opera.' },
+						{ role: 'user', content: body?.prompt },
+					],
+				};
+				let response = await ai.run('@cf/meta/llama-2-7b-chat-int8', chat);
+				tasks.push({ inputs: chat, response });
+				return Response.json(tasks);
+			}
+		}
+		return Response.json({ error: 'needs to be a POST request with {"prompt": <prompt>}' });
 	},
 };
